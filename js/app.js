7ba1da25bd7e2b4afe7abb36cfc58a6e41c7172b
nicholasbair <nicholasbair@Nicholass-MBP.home>
2016-01-02 13:34:45 -0500

    More clean up

--- js/app.js
+++ js/app.js
@@ -26,8 +26,6 @@ Enemy.prototype.render = function() {
 };
 
 // Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 var Player = function() {
     this.sprite = 'images/char-boy.png';
     this.x = 200;
